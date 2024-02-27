283ea4113f3a69bea2374cef278e75237aaa6053
jjsshhhhh <108526743+jjsshhhhh@users.noreply.github.com>
2024-02-27 11:07:26 +0900

    누적합

--- 누적합/J_11659.java
+++ 누적합/J_11659.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {	
     public static void main(String[] args) throws IOException {
